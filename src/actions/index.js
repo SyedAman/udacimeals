b68668f2b06bf932e9a567ceffdf7dec51dafb17
Syed Aman <sna274@nyu.edu>
2017-12-20 12:21:49 +0000

    create actions

---
+++ src/actions/index.js
@@ -0,0 +1,21 @@
+// define action types
+export const ADD_RECIPE = 'ADD_RECIPE';
+export const REMOVE_FROM_CALENDER = 'REMOVE_FROM_CALENDER';
+
+// define action creators
+export function addRecipe({ day, recipe, meal }) {
+	return {
+		type: ADD_RECIPE,
+		day,
+		recipe,
+		meal,
+	};
+}
+
+export function removeFromCalender({ day, meal }) {
+	return {
+		type: REMOVE_FROM_CALENDER,
+		day,
+		meal,
+	};
+}
