7d9f600a24fbd8d6ba050e7bfe704f1cdc5c2729
Syed Aman <sna274@nyu.edu>
2018-01-12 22:59:32 -0600

    improve documentation

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,14 @@
 export const ADD_RECIPE = 'ADD_RECIPE';
 export const REMOVE_FROM_CALENDER = 'REMOVE_FROM_CALENDER';
 
-// define action creators
+/**
+ * Action creator that creates an action indicating an a new recipe.
+ * @method addRecipe
+ * @param  {string} day - The day of the recipe.
+ * @param  {string} recipe - The steps to cook the meal.
+ * @param  {string} meal - The name of the meal.
+ * @return {Object} - The action.
+ */
 export function addRecipe({ day, recipe, meal }) {
 	return {
 		type: ADD_RECIPE,
@@ -12,6 +19,14 @@ export function addRecipe({ day, recipe, meal }) {
 	};
 }
 
+/**
+ * Action creator that creates an action indicating removing a meal from the
+ * calender.
+ * @method removeFromCalender
+ * @param  {string} day - The the meal is served.
+ * @param  {string} meal - The name of the meal.
+ * @return {Object} - The action.
+ */
 export function removeFromCalender({ day, meal }) {
 	return {
 		type: REMOVE_FROM_CALENDER,
