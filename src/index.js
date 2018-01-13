493f32af02c56b43b5dfc13b5404d7cfedaf36e5
Syed Aman <sna274@nyu.edu>
2018-01-13 15:13:49 -0600

    move store backinto index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
-import './store';
+import reducer from './reducers';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+/**
+ * The application state made using Redux.
+ * @type {object}
+ */
+const store = createStore(
+	reducer,
+	// integrate store with Redux Dev Tools
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
