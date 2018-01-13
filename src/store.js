5f5c0e8a4f79df562dc64e08cd3d61a96f2e981d
Syed Aman <sna274@nyu.edu>
2018-01-13 15:12:32 -0600

    add docs for store

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { createStore } from 'redux';
 
 import reducer from './reducers';
 
-// create the redux store
+/**
+ * The application state made using Redux.
+ * @type {object}
+ */
 const store = createStore(
 	reducer,
 	// integrate store with Redux Dev Tools
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-
