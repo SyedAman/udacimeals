9b876bb49c9985b090f1db80d2f23fd17a16b7b0
Syed Aman <sna274@nyu.edu>
2018-02-03 21:05:35 -0600

    create a reducer that handles food state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,28 @@
 import { ADD_RECIPE, REMOVE_FROM_CALENDER } from '../actions';
 
 /**
+ * Returns an updated version of the state in relation to food state, based on
+ * the food action received.
+ * @method foodReducer
+ * @param  {Object} [state={}] - The redux store.
+ * @param  {Object} action - The payload.
+ * @return {Object} - An updated version of the state.
+ */
+const foodReducer = (state = {}, action) => {
+	const { recipe } = action;
+
+	switch (action.type) {
+		case ADD_RECIPE:
+			return {
+				...state,
+				[recipe.label]: recipe,
+			};
+		default:
+			return state;
+	}
+};
+
+/**
  * The shape/format of the application state.
  * @type {Object}
  */
