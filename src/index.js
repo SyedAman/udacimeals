65ab882c773a2246aec24fba21c37312d23f2b30
Syed Aman <sna274@nyu.edu>
2018-02-10 19:17:27 -0600

    inject a middleware into the redux store that logs actions and the updated state after the action is dispatched

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+const actionLoggerMiddleware = store => next => action => {
+	console.group(action.type);
+	console.log('dispatching', action);
+
+	const result = next(action);
+
+	console.log('next state', store.getState());
+	console.groupEnd(action.type);
+
+	return result;
+};
+
+const composeEnhancersWithReduxDevTools =
+	window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
 
 /**
  * The application state made using Redux.
@@ -15,7 +30,7 @@ import registerServiceWorker from './registerServiceWorker';
 const store = createStore(
 	rootReducer,
 	// integrate store with Redux Dev Tools
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+	composeEnhancersWithReduxDevTools(applyMiddleware(actionLoggerMiddleware)),
 );
 
 ReactDOM.render(
